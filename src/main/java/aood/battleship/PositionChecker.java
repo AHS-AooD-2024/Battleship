25aa6153359f812abf518a7eda7c09238707373b
nathanli5722 <152818045+nathanli5722@users.noreply.github.com>
2024-08-28 13:15:23 +0000

    Might work. Coded as specified.

---
+++ src/main/java/aood/battleship/PositionChecker.java
@@ -0,0 +1,38 @@
+package aood.battleship;
+
+/**
+ * A PositionChecker for battleship within A-J / 1-10
+ * 
+ * @author Nathan Li
+ */
+public class PositionChecker
+{
+    /**
+     * Checks if a position is valid within A-J & 1-10
+     *  
+     * @param inputPos a position in format row-column eg. (A-1)
+     * @return a Position object. Invalid position returns Position indicies (-1,-1)
+     */
+    public static Position checkPosition(String inputPos)
+    {
+        String[] input = inputPos.split("-");
+        
+        if (input.length == 2)
+        {
+            char row = input[0].charAt(0);
+            int col = Integer.parseInt(input[1]);
+
+            if (row - 'A' >= 0 && 
+                row - 'A' < 10 &&
+                col > 0 &&
+                col <= 10
+                )
+            {
+                return new Position(row, col);
+            }
+        }
+
+        //return for invalid format
+        return new Position(-1, -1);
+    }
+}
