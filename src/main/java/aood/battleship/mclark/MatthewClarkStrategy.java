e3f623b09791c0a2196ba396f615d80735107649
Matthew <117416222+Something-Inconspicuous@users.noreply.github.com>
2024-11-15 08:51:22 -0500

    Fixed it

--- src/main/java/aood/battleship/mclark/MatthewClarkStrategy.java
+++ src/main/java/aood/battleship/mclark/MatthewClarkStrategy.java
@@ -41,18 +41,53 @@ public class MatthewClarkStrategy extends BasePlayer {
 
     @Override
     public Position getShot() {
-        return elvis(moves.poll(), this::mostLikely);
+        Position shot;
+        if(!goodToShoot(shot = elvis(moves.poll(), this::mostLikely))) {
+            System.out.println("Repeat shot: " + shot);
+        }
+
+        if((mode & ALL_SEARCH) != 0) {
+            if(shot.equals(up))
+                currentDirection = UP_SEARCH;
+            else if(shot.equals(down))
+                currentDirection = DOWN_SEARCH;
+            else if(shot.equals(left))
+                currentDirection = LEFT_SEARCH;
+            else if(shot.equals(right))
+                currentDirection = RIGHT_SEARCH;
+        }
+
+        return shot;
     }
 
     private Position lastShot;
     private boolean lastShotDidHit;
+    private int mode;
+
+    private static final int MOST_LIKELY =  1 << 0;
+    private static final int LEFT_SEARCH =  1 << 1;
+    private static final int RIGHT_SEARCH = 1 << 2;
+    private static final int UP_SEARCH =    1 << 3;
+    private static final int DOWN_SEARCH =  1 << 4;
+    private static final int HORIZONTAL_SEARCH = LEFT_SEARCH | RIGHT_SEARCH;
+    private static final int VERTICAL_SEARCH = UP_SEARCH | DOWN_SEARCH;
+    private static final int ALL_SEARCH = HORIZONTAL_SEARCH | VERTICAL_SEARCH;
+
+    private Position up;
+    private Position right;
+    private Position down;
+    private Position left;
+
+    private int currentDirection;
 
     @Override
     protected void onShoot(HitInfo hitInfo) {
+        reprobulate();
+
         // System.out.println("Shot: " + hitInfo.getPos());
         // for(int r = 0; r < 10; r++) {
         //     for(int c = 0; c < 10; c++) {
-        //         System.out.print(chance[r][c]);
+        //         System.out.printf("%3d", chance[r][c]);
         //     }
         //     System.out.println();
         // }
@@ -65,54 +100,116 @@ public class MatthewClarkStrategy extends BasePlayer {
             moves.clear();
 
             if(hitInfo.isSunk()) {
+                mode = MOST_LIKELY;
                 moves.clear();
                 moves.offer(mostLikely());
-            } else if(lastShotDidHit) {
-                // FIXME this is bad and doesn't work.
-                // suppossed to continue shooting in the same direction until
-                // sunk
-                // also should go to the oposite side if we miss and the boat is
-                // not sunk. That is why the previous if just cancels everything if we sink the boat.
-                int rowDiff = hitInfo.getPos().getRowIndex() - lastShot.getRowIndex();
-                int colDiff = hitInfo.getPos().getColIndex() - lastShot.getColIndex();
-            
-                Position next = new Position(
-                    hitInfo.getPos().getRowIndex() + rowDiff, 
-                    hitInfo.getPos().getColIndex() + colDiff 
-                );
+            } else if(lastShotDidHit && (mode & ALL_SEARCH) != 0) {
+                // use current direction
+                moves.clear();
 
-                if(goodToShoot(next))
-                    moves.offer(next);
+                Position _up = new Position(hitInfo.getPos().getRowIndex(), hitInfo.getPos().getColIndex() - 1);
+                Position _left = new Position(hitInfo.getPos().getRowIndex() - 1, hitInfo.getPos().getColIndex());
+                Position _right = new Position(hitInfo.getPos().getRowIndex() + 1, hitInfo.getPos().getColIndex());
+                Position _down = new Position(hitInfo.getPos().getRowIndex(), hitInfo.getPos().getColIndex() + 1);
+
+                Position finale = null;
+
+                switch (currentDirection) {
+                    case UP_SEARCH:
+                        finale = _up;
+                        break;
+                    case DOWN_SEARCH:
+                        finale = _down;
+                        break;
+                    case LEFT_SEARCH:
+                        finale = _left;
+                        break;
+                    case RIGHT_SEARCH:
+                        finale = _right;
+                        break;
+                
+                    default:
+                        throw new IllegalStateException("Current search direction " + currentDirection);
+                }
+
+                if(goodToShoot(finale)) {
+                    moves.offer(finale);
+                } else {
+                    mode &= ~currentDirection;
+                }
+
             } else {
                 // This is if this was the discovery shot of a boat.
-                Position up = new Position(hitInfo.getPos().getRowIndex(), hitInfo.getPos().getColIndex() - 1);
-                Position left = new Position(hitInfo.getPos().getRowIndex() - 1, hitInfo.getPos().getColIndex());
-                Position right = new Position(hitInfo.getPos().getRowIndex() + 1, hitInfo.getPos().getColIndex());
-                Position down = new Position(hitInfo.getPos().getRowIndex(), hitInfo.getPos().getColIndex() + 1);
+                up = new Position(hitInfo.getPos().getRowIndex(), hitInfo.getPos().getColIndex() - 1);
+                left = new Position(hitInfo.getPos().getRowIndex() - 1, hitInfo.getPos().getColIndex());
+                right = new Position(hitInfo.getPos().getRowIndex() + 1, hitInfo.getPos().getColIndex());
+                down = new Position(hitInfo.getPos().getRowIndex(), hitInfo.getPos().getColIndex() + 1);
 
-                if(goodToShoot(up)) moves.offer(up);
-                if(goodToShoot(left)) moves.offer(left);
-                if(goodToShoot(right)) moves.offer(right);
-                if(goodToShoot(down)) moves.offer(down);
+                if(goodToShoot(up)){ 
+                    moves.offer(up);
+                    mode |= UP_SEARCH;
+                }
+
+                if(goodToShoot(left)){
+                    moves.offer(left);
+                    mode |= LEFT_SEARCH;
+                }
+
+                if(goodToShoot(right)) {
+                    moves.offer(right);
+                    mode |= RIGHT_SEARCH;
+                }
+                if(goodToShoot(down)) {
+                    moves.offer(down);
+                    mode |= DOWN_SEARCH;
+                }
             }
         } else {
-            if(!lastShotDidHit)
-                moves.offer(mostLikely());
+            // if miss
+            
+            // remove the direction from searches
+            mode &= ~currentDirection;
+            
         }
         lastShot = hitInfo.getPos();
         lastShotDidHit = hitInfo.isHit();
-        reprobulate();
+
+        // System.console().readLine("Enter to coninue.");
     }
 
     private Position mostLikely() {
         int bestR = 0;
         int bestC = 0;
-        // O(n^2) unfortunately
-        for(int r = 1; r < chance.length; r++) {
-            for(int c = 0; c < chance[0].length; c++) {
-                if(goodToShoot(new Position(r, c)) && chance[r][c] > chance[bestR][bestC]) {
+
+        int bestChance = Integer.MIN_VALUE;
+
+        int r = 0, c = 0;
+        boolean superBreak = true;
+
+        for(r = 0; r < chance.length && superBreak; r++) {
+            for(c = 0; c < chance[0].length; c++) {
+                if(goodToShoot(new Position(r, c))) {
                     bestR = r;
                     bestC = c;
+                    bestChance = chance[bestR][bestC];
+                    superBreak = false;
+                    break;
+                }
+            }
+        }
+        
+        // O(n^2) unfortunately
+        for(; r < chance.length; r++) {
+            for(c = 0; c < chance[0].length; c++) {
+                if(chance[r][c] >= bestChance){
+                    if(goodToShoot(new Position(r, c))) {
+                        bestR = r;
+                        bestC = c;
+                        bestChance = chance[bestR][bestC];
+                    }
+                    // else {
+                    //     System.out.println("Rejected " + new Position(r, c));
+                    // }
                 }
             }
         }
@@ -140,8 +237,8 @@ public class MatthewClarkStrategy extends BasePlayer {
         for(int c = 0; c < chance[0].length - 1; c++) {
             // the general algorithm doesn't do this, so we'll manually
             // zero out already shot positions
-            if(getGrid().isMiss(new Position(r, c))) {
-                chance[r][c] = 0;
+            if(!getGrid().isEmpty(new Position(r, c))) {
+                chance[r][c] = -99;
                 continue;
             }
             for(int search = 1; search < shipsWithLength.length; search++) {
@@ -160,8 +257,8 @@ public class MatthewClarkStrategy extends BasePlayer {
         for(int r = 0; r < chance.length - 1; r++) {
             // the general algorithm doesn't do this, so we'll manually
             // zero out already shot positions
-            if(getGrid().isMiss(new Position(r, c))) {
-                chance[r][c] = 0;
+            if(!getGrid().isEmpty(new Position(r, c))) {
+                chance[r][c] = -99;
                 continue;
             }
 
